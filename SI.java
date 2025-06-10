b44b3c8ce6e60eafe52db7474ac0aec2a0db7d19
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-06-10 14:50:48 +0530

    save

---
+++ SI.java
@@ -0,0 +1,21 @@
+ class Simple_Interest{
+    public double P,T,R,S_I;
+
+    Simple_Interest(double P,double T,double R)
+    {
+        this.P=P;
+        this.T=T;
+        this.R=R;
+    }
+    public void calculate_SI()
+    {
+        S_I=(P*T*R)/100;
+        System.out.println("Simple Interest:"+S_I);
+    }
+}
+public class main{
+    public static void main(String[] args) {
+        Simple_Interest s = new Simple_Interest(1000, 2, 15.00);
+        s.calculate_SI();
+    }
+}
